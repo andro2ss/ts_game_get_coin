a0e236826b59e4c41b3cfa3a6757040479bf3cc8
andro2ss <a.brzeski@hotmail.com>
2022-04-09 23:06:08 +0200

    added author footer, home page and game page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import FooterAB from "./common/footer/FooterAB";
+import { Container } from "@mui/material";
+import Home from "./pages/home/Home";
+import Game from "./pages/game/Game";
 
 function App() {
   return (
-   <>init</>
+    <Container className="app__container">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/play" element={<Game />} />
+        </Routes>
+      </BrowserRouter>
+      <FooterAB githubLink="https://github.com/andro2ss/ts_game_get_coin" />
+    </Container>
   );
 }
 
