7b6aa5ddefbbb1ca52ba68230c0fb6abefd063c1
andro2ss <a.brzeski@hotmail.com>
2022-04-10 21:08:18 +0200

    started work with game page and styling

--- src/components/pages/game/Game.tsx
+++ src/components/pages/game/Game.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { ReactElement, useEffect, useState } from "react";
+import "./Game.scss";
+import { GameField } from "./items/GameField";
 
 function Game() {
-  return <div>PLAY</div>;
+  const [gameFields, setGameFields] = useState<any>();
+
+  useEffect(() => {
+    if (!gameFields) {
+      let tempMap = [];
+      for (let i = 0; i < 100; i++) {
+        tempMap.push(<GameField key={"gameField" + i} fieldNr={i} />);
+      }
+      setGameFields(tempMap);
+    }
+  }, [gameFields]);
+  return (
+    <div className="game__container">
+      {!gameFields
+        ? "tak"
+        : gameFields.map((item: ReactElement) => {
+            return item;
+          })}
+    </div>
+  );
 }
 
 export default Game;
