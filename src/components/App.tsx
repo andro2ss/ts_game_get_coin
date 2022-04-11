42e9e170a0fe12a3541910952fecab3d9478f42c
andro2ss <a.brzeski@hotmail.com>
2022-04-11 12:58:03 +0200

    created result page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,6 +5,7 @@ import { Container } from "@mui/material";
 import "animate.css";
 import Home from "./pages/home/Home";
 import Game from "./pages/game/Game";
+import Result from "./pages/result/Result";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/play" element={<Game />} />
+          <Route path="/result" element={<Result />} />
         </Routes>
       </BrowserRouter>
       <FooterAB githubLink="https://github.com/andro2ss/ts_game_get_coin" />
