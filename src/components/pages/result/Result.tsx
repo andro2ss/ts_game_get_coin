e0b65c17fabfb02a4685fd2c2ce5760e2e4e27a0
andro2ss <a.brzeski@hotmail.com>
2022-04-11 15:26:49 +0200

    added user score

--- src/components/pages/result/Result.tsx
+++ src/components/pages/result/Result.tsx
@@ -1,7 +1,11 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { UserScore } from "../../../redux/reducers/userScore";
 
 function Result() {
-  return <div>Twój wynik:</div>;
+  const userScore = useSelector((state: UserScore) => state.userScore);
+
+  return <div>Twój wynik: {userScore}</div>;
 }
 
 export default Result;
