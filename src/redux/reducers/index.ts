28d6d3574a05ae28191043f3be25a87ec07bea8b
andro2ss <a.brzeski@hotmail.com>
2022-04-12 14:18:53 +0200

    added new reducers

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,14 +1,18 @@
 import { combineReducers } from "redux";
-import { gameUser } from "./gameUser";
+import { gameUserName } from "./gameUserName";
 import { userPos } from "./userPos";
 import { userScore } from "./userScore";
 import { targetPos } from "./targetPos";
+import { gameRound } from "./gameRound";
+import { gameUserDirection } from "./gameUserDirection";
 
 const allReducers = combineReducers({
-  gameUser,
+  gameUserName,
   userPos,
   userScore,
   targetPos,
+  gameRound,
+  gameUserDirection,
 });
 
 export default allReducers;
