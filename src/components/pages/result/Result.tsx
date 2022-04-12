2baf7eb954316c798c23ab70b14b9a6c8c311671
andro2ss <a.brzeski@hotmail.com>
2022-04-12 14:22:07 +0200

    fixed btn play again

--- src/components/pages/result/Result.tsx
+++ src/components/pages/result/Result.tsx
@@ -1,11 +1,32 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import { UserScore } from "../../../redux/reducers/userScore";
+import { useDispatch, useSelector } from "react-redux";
+import { setUserScore, UserScore } from "../../../redux/reducers/userScore";
+import { Link } from "react-router-dom";
+import { setTargetPos } from "../../../redux/reducers/targetPos";
+import { setUserPos } from "../../../redux/reducers/userPos";
+import { setGameRound } from "../../../redux/reducers/gameRound";
 
 function Result() {
   const userScore = useSelector((state: UserScore) => state.userScore);
+  const dispatch = useDispatch();
 
-  return <div>Twój wynik: {userScore}</div>;
+  return (
+    <div>
+      Twój wynik: {userScore}
+      <button
+        onClick={() => {
+          dispatch(setUserScore(-1));
+          dispatch(setTargetPos(55));
+          dispatch(setUserPos(55));
+          dispatch(setGameRound(1));
+        }}
+      >
+        <Link to="/play" className="logged__btn logged__btn--start">
+          <span>Jeszcze raz</span>
+        </Link>
+      </button>
+    </div>
+  );
 }
 
 export default Result;
