7579fda612f65703f0a4e6847858e10612bc66b5
andro2ss <a.brzeski@hotmail.com>
2022-04-13 14:31:09 +0200

    fixed generating monsters

--- src/components/pages/game/helpers/newMonsterPosition.ts
+++ src/components/pages/game/helpers/newMonsterPosition.ts
@@ -3,22 +3,23 @@ export default function newMonsterPosition(
   targetPos: number,
   monsterAmount: number
 ) {
-  let newPosition: number = Math.floor(Math.random() * 100);
   let monsters: number[] = [];
-
-  while (
-    userPos === newPosition ||
-    userPos - 1 === newPosition ||
-    userPos - 10 === newPosition ||
-    userPos + 1 === newPosition ||
-    userPos + 10 === newPosition ||
-    targetPos === newPosition ||
-    targetPos - 1 === newPosition ||
-    targetPos - 10 === newPosition ||
-    targetPos + 1 === newPosition ||
-    targetPos + 10 === newPosition
-  ) {
-    newPosition = Math.floor(Math.random() * 100);
+  while (monsters.length < monsterAmount) {
+    let newPosition: number = Math.floor(Math.random() * 100);
+    let positionCheck =
+      userPos === newPosition ||
+      userPos - 1 === newPosition ||
+      userPos - 10 === newPosition ||
+      userPos + 1 === newPosition ||
+      userPos + 10 === newPosition ||
+      targetPos === newPosition ||
+      targetPos - 1 === newPosition ||
+      targetPos - 10 === newPosition ||
+      targetPos + 1 === newPosition ||
+      targetPos + 10 === newPosition;
+    if (monsters.indexOf(newPosition) === -1 && !positionCheck)
+      monsters.push(newPosition);
   }
+
   return monsters;
 }
