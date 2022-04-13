a98fef6563058f4bf72344171861053d738540fc
andro2ss <a.brzeski@hotmail.com>
2022-04-13 14:31:24 +0200

    added monster position to logic

--- src/components/pages/game/helpers/newTargetPosition.ts
+++ src/components/pages/game/helpers/newTargetPosition.ts
@@ -1,11 +1,15 @@
-export default function newTargetPosition(userPos: number) {
+export default function newTargetPosition(
+  userPos: number,
+  monstersPos: number[]
+) {
   let newPosition: number = Math.floor(Math.random() * 100);
   while (
     userPos === newPosition ||
     userPos - 1 === newPosition ||
     userPos - 10 === newPosition ||
     userPos + 1 === newPosition ||
-    userPos + 10 === newPosition
+    userPos + 10 === newPosition ||
+    monstersPos.indexOf(newPosition) !== -1
   ) {
     newPosition = Math.floor(Math.random() * 100);
   }
