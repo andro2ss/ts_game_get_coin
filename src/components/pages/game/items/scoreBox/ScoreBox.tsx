1701cdc129af962d083fecd24af58a03c1041a04
andro2ss <a.brzeski@hotmail.com>
2022-04-13 14:32:46 +0200

    created scoreBox component

--- src/components/pages/game/items/scoreBox/ScoreBox.tsx
+++ src/components/pages/game/items/scoreBox/ScoreBox.tsx
@@ -1,17 +1,22 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { UserScore } from "../../../../redux/reducers/userScore";
-import { setEndTime } from "../../../../helpers/timeCounter/setEndTime";
-import timeCounter from "../../../../helpers/timeCounter/timeCounter";
-import { GameRound, setGameRound } from "../../../../redux/reducers/gameRound";
-import { setMonstersPos } from "../../../../redux/reducers/monstersPos";
-import newMonsterPosition from "../helpers/newMonsterPosition";
-import { UserPosition } from "../../../../redux/reducers/userPos";
-import { TargetPosition } from "../../../../redux/reducers/targetPos";
+import { UserScore } from "../../../../../redux/reducers/userScore";
+import { setEndTime } from "../../../../../helpers/timeCounter/setEndTime";
+import timeCounter from "../../../../../helpers/timeCounter/timeCounter";
+import {
+  GameRound,
+  setGameRound,
+} from "../../../../../redux/reducers/gameRound";
+import { setMonstersPos } from "../../../../../redux/reducers/monstersPos";
+import newMonsterPosition from "../../helpers/newMonsterPosition";
+import { UserPosition } from "../../../../../redux/reducers/userPos";
+import { TargetPosition } from "../../../../../redux/reducers/targetPos";
+import "./ScoreBox.scss";
 
 export function ScoreBox() {
   const [tempEndDate, setTempEndDate] = useState<Date>();
   const [timer, setTimer] = useState<number>();
+  const [stateControl, setStateControl] = useState<number>(0);
   const dispatch = useDispatch();
 
   const userScore = useSelector((state: UserScore) => state.userScore);
@@ -33,17 +38,21 @@ export function ScoreBox() {
       }
 
       if (timer && timer <= 0) {
-        setTempEndDate(setEndTime());
-        dispatch(setGameRound(currentRound + 1));
-        dispatch(
-          setMonstersPos(
-            newMonsterPosition(userPosition, targetPosition, currentRound)
-          )
-        );
+        setStateControl(1);
+        if (stateControl === 1) {
+          setTempEndDate(setEndTime());
+          dispatch(setGameRound(currentRound + 1));
+          dispatch(
+            setMonstersPos(
+              newMonsterPosition(userPosition, targetPosition, currentRound)
+            )
+          );
+          setStateControl(0);
+        }
       }
     }, 100);
     return () => clearInterval(timeInterval);
-  }, [timer, tempEndDate]);
+  }, [timer, tempEndDate, stateControl]);
 
   return (
     <div className="game__scoreBox">
